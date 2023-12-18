070e2e17d39c721c3a3c4dda6b7431e692c2e8da
MinhQuyennn <20521824@gm.uit.edu.vn>
2023-12-18 22:47:49 +0700

    v

--- ex1_3/src/App.js
+++ ex1_3/src/App.js
@@ -1,10 +1,13 @@
+
 import Router from "./react1/routes";
 
 
 function App() {
   return (
     <div className="App">
+
       <Router />
+
     </div>
   );
 }
--- ex1_3/src/react1/routes/index.js
+++ ex1_3/src/react1/routes/index.js
@@ -1,25 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import BlankLayout from "../layouts/black";
-import HomeLayout from "../layouts/home";
+// import BlankLayout from "../layouts/blank/index";
+// import HomeLayout from "../layouts/home/index";
 
-import Home from "../pages/home";
-import Shop from "../pages/shop";
-import Contact from "../pages/contact";
+import Home from "../pages/home/index";
+import Shop from "../pages/shop/index";
+import Contact from "../pages/contact/index";
 
-import Login from "../pages/login";
+import Login from "../pages/login/index";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeLayout />}>
+        {/* <Route path="/" element={<HomeLayout />}> */}
           <Route path="/" element={<Home />} />
           <Route path="shop" element={<Shop />} />
           <Route path="contact" element={<Contact />} />
-        </Route>
-        <Route path="/" element={<BlankLayout />}>
-          <Route path="login" element={<Login />} />
-        </Route>
+        {/* </Route> */}
+        {/* <Route path="/" element={<BlankLayout />}>
+          <Route path="login" element={<Login />} /> */}
+        {/* </Route> */}
       </Routes>
     </BrowserRouter>
   );
